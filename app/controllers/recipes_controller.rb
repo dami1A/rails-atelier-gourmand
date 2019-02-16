e5fe7c31fa53e50dd39e4426fe15cf126ab7d2d5
damien arnaud <darnaud@mail.com>
2019-02-17 00:08:41 +0100

    recipe and ingredients done

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,29 @@
 class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+  end
+
+  # def show
+  #   @recipe = recipe.find(params[:id])
+  #   @doses = Dose.where(recipe_id: @recipe.id)
+  # end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to recipes_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :prep_time, :cook_time, :portions, :category)
+  end
 end
