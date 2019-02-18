58052e5f15f490685bbbad0db9d729a8d9534150
damien arnaud <darnaud@mail.com>
2019-02-18 18:58:10 +0100

    JS ok, including select2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,7 +13,7 @@ import 'bootstrap';
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 import 'select2/dist/css/select2.css';
 import { initSelect2 } from '../components/init_select2';
-import { previewImageOnFileSelect } from '../components/photo-preview';
+import { previewImageOnFileSelect } from '../components/photo_preview';
 
 initUpdateNavbarOnScroll();
 initSelect2();
--- app/models/instruction.rb
+++ app/models/instruction.rb
@@ -7,7 +7,7 @@ class Instruction < ApplicationRecord
   validates :description, presence: true
   validates :step,
             presence: true,
-            inclusion: { in: STEPS, error: "tu dois choisir un numéro d'étape pour ton instruction" }
+            inclusion: { in: STEPS, message: "tu dois choisir un numéro d'étape pour ton instruction" }
   validates :recipe_id, presence: true, null: false
   validates :recipe_id, uniqueness: { scope: :step,
     message: "Tu ne peux pas ajouter 2 fois le même numéro d'étape dans une recette" }
