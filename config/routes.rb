83f3c49b3c2cc4d17390d09480231bb606888031
damien arnaud <darnaud@mail.com>
2019-02-19 02:44:59 +0100

    user done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
   resources :recipes, only: [:index, :create, :new, :show, :destroy] do
     resources :doses, only: [:create]
     resources :instructions, only: [:create]
